b1bd5565e0d713a6523c47fa898df12ccc542d3f
SixGangs <2577944135@qq.com>
2017-05-19 16:44:19 +0800

    一次性全部提交

--- main/main.js
+++ main/main.js
@@ -1,18 +1,49 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.Elements()}
+o) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
+	// 构造函数
   constructor(input) {
-    // Write your code here
+    this.data = input;
   }
-
+  
+  // 求序列最小值的函数
   minimum() {
-    // Write your code here
+    var min = Number.POSITIVE_INFINITY;
+    for(var i = 0; i < this.data.length; i++){
+    	if(this.data[i] < min)
+    	min = this.data[i];
+    }
+    return min;
   }
 
-  // Write your code here
+  // 求序列最大值的函数
+  maximum() {
+    var max = Number.NEGATIVE_INFINITY;
+    for(var i = 0; i < this.data.length; i++){
+    	if(this.data[i] > max)
+    	max = this.data[i];
+    }
+    return max;
+  }
+  
+  // 求序列元素个数的函数
+  Elements(){
+  	return this.data.length;
+  }
+  
+   // 求序列平均值的函数
+   average() {
+    var ave = 0.0;
+    for(var i = 0; i < this.data.length; i++){
+    	ave = ave + this.data[i];
+    }
+    ave = ave / this.data.length;
+    return ave.toFixed(2);;
+  }
 }
